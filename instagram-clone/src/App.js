b34a303fefd5806d55affee430ec6e8444b57e7c
Volodymyr <syvochka64@gmail.com>
2021-04-08 17:46:44 +0200

    Added a modal

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -2,9 +2,38 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import Post from "./Post";
 import { db } from "./firebase.js";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import { Button } from "@material-ui/core";
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
+
   const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     db.collection("posts").onSnapshot((snapshot) => {
@@ -17,8 +46,16 @@ function App() {
     });
   }, []);
 
+  const signUp = (event) => {};
+
   return (
-    <div className="App">
+    <div className="app">
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <h2>I am a modal</h2>
+        </div>
+      </Modal>
+
       <div className="app__header">
         <img
           className="app__headerImage"
@@ -26,6 +63,14 @@ function App() {
           alt=""
         />
       </div>
+
+      <Button
+        onClick={() => {
+          setOpen(true);
+        }}
+      >
+        Sign Up
+      </Button>
 
       <h1>Instagram Clone</h1>
 
